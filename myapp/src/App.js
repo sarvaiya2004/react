e4808c4f23c99e68ce2f83ef48d9db8cc4a04905
sarvaiya2004 <123886258+sarvaiya2004@users.noreply.github.com>
2023-04-02 05:16:53 +0530

    react
    red

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Person from './Components/Person/Person';
+
+
+function App() {
+  return (
+    <div className='App'>
+      <Person />
+      <Person />
+     
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/Components/Person/Person.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+ 
+
+
+class Person extends Component {
+   // constructor(props) { 
+     //   super(props);
+   // }
+    render(){
+        return(
+            <div>
+              hader
+          <h1>
+      
+        red store
+          </h1>
+                
+            </div>
+        )
+    }
+}
+
+export default Person;
+ 
